1a83b85f4c67f76fd57b7fc2c63475e73effe2e8
姚观寿 <yaoguanshou@rainbowcn.com>
2022-08-04 09:47:40 +0800

    修改多进程配置

--- server/index.js
+++ server/index.js
@@ -2,15 +2,16 @@
  * @Date: 2022-08-01 17:17:17
  * @Author: Yao guan shou
  * @LastEditors: Yao guan shou
- * @LastEditTime: 2022-08-04 09:23:55
+ * @LastEditTime: 2022-08-04 09:47:30
  * @FilePath: /react-loading-ssr/server/index.js
  * @Description:
  */
 import cluster from 'cluster'
 import App from './app'
-
+import os from 'os'
+const { Worker } = require('worker_threads')
 // 开启的子进程数
-const workerNum = 3 
+const workerNum = os.cpus().length
 // 如果是主进程
 if (cluster.isMaster) {
   // 创建子进程
@@ -18,7 +19,10 @@ if (cluster.isMaster) {
     // 通过cluster.fork创建子进程
     cluster.fork()
   }
-  // 如果有子进程，就启动相关服务,这里会使用三个进程来执行http服务演示
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`) 
+  })
+  // 如果有子进程，就启动相关服务,这里会使用cpu多个进程来执行http服务 
 } else {
   new App()
 }
